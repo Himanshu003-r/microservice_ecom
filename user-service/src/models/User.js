dee5aaf5b81d2c6495b78bac668a48fd6e44c4dd
Himanshu Rawat <himanshur651@gmail.com>
2025-12-19 13:26:23 +0530

    added user-service models

---
+++ user-service/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user','admin'],
+        default: 'user'
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    }
+},{timestamps: true})
+
+// Pre hook middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next;
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.index({username: 'text'})
+
+const User = mongoose.model('User',userSchema)
+export default User
