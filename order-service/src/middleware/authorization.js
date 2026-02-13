7e8034d0e870055011e538bb2b2fa0d5b65dacd7
Himanshu Rawat <himanshur651@gmail.com>
2026-02-13 20:19:11 +0530

    fixed bug

--- order-service/src/middleware/authorization.js
+++ order-service/src/middleware/authorization.js
@@ -1,32 +1,32 @@
-import ApiError from "../errors/customAPIError.js"
-import logger from "../utils/logger.js"
+import ApiError from "../errors/customAPIError.js";
+import logger from "../utils/logger.js";
 
-export const authorizeRoles = (...roles)=> {
-    return (req,res,next)=> {
-        if(!req.user){
-            logger.warn('Unauthorized access')
-            throw new ApiError(401,'Unauthorized access')
-        }
-
-        if(!roles.includes(req.user.role)){
-                        logger.warn('Unauthorized access')
-            throw new ApiError(401,'Unauthorized access')
-        }
-            next()
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      logger.warn("Unauthorized access");
+      throw new ApiError(401, "Unauthorized access");
     }
-}
+
+    if (!roles.includes(req.user.role)) {
+      logger.warn("Unauthorized access");
+      throw new ApiError(401, "Unauthorized access");
+    }
+    next();
+  };
+};
 
 export const checkOwnership = (resourceUserId) => {
   return (req, res, next) => {
     // Admin can access anything
-    if (req.user.role === 'admin') {
+    if (req.user.role === "admin") {
       return next();
     }
 
-    // Check if user owns the resource
+    // Checking if user owns the resource
     if (req.user.userId !== resourceUserId.toString()) {
       throw new CustomErrors.UnauthorizedError(
-        'Not authorized to access this resource'
+        "Not authorized to access this resource",
       );
     }
 
