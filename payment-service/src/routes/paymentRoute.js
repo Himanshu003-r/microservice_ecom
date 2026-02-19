853c39bc9a467690f9453cbf7ea0e691c20e91aa
Himanshu Rawat <himanshur651@gmail.com>
2026-02-19 13:58:31 +0530

    added payment route

--- payment-service/src/routes/paymentRoute.js
+++ payment-service/src/routes/paymentRoute.js
@@ -1,15 +1,11 @@
 import {Router} from 'express'
 import {getPaymentByOrder,getPaymentStatus} from '../controllers/paymentController.js'
 import authenticate from '../middleware/verifyGateway.js'
-import {authorizeRoles} from '../middleware/authorization.js'
-
-
 
 const router = Router()
 
 router.get('/order/:orderId',authenticate,getPaymentByOrder)
 router.get('/:id',authenticate,getPaymentStatus)
-// router.get('/stats',authenticate,authorizeRoles('admin'),getOrderStats)
 
 export default router
 
